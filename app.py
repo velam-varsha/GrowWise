6f7c0eff2c7408b6b7b94c932c5dce203ff36099
velam-varsha <120495909+velam-varsha@users.noreply.github.com>
2023-05-19 10:14:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 from flask import Flask, render_template, request, Markup
 import numpy as np
 import pandas as pd
-from utils.disease import disease_dic
 from utils.fertilizer import fertilizer_dic
 import requests
 import config
